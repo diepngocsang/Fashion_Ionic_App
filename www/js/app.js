b60e2b34b8870d4f11a07ad6d186557e3398a3d5
diepngocsang <sangdngc60231@gmail.com>
2016-05-09 11:06:03 +0700

    remove template playlist in app.js

--- www/js/app.js
+++ www/js/app.js
@@ -70,25 +70,8 @@ angular.module('starter', ['ionic','firebase'])
           templateUrl: 'templates/home.html'
         }
       }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+    });
+    
   $urlRouterProvider.otherwise('/app/home');
 })
 .constant('GET_URL','https://finalassignment.firebaseio.com/');
